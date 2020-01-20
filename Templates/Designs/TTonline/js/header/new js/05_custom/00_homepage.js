6347bb78d15a4a5224086b305a622d72f1e09094
Monica Munteanu <36918236+MollyTheCoder@users.noreply.github.com>
2020-01-20 17:41:54 +0200

    - recomandari TT(1)

--- Templates/Designs/TTonline/js/header/new js/05_custom/00_homepage.js
+++ Templates/Designs/TTonline/js/header/new js/05_custom/00_homepage.js
@@ -11,29 +11,51 @@ document.addEventListener('DOMContentLoaded', function(event) {
 //On page load, first items loaded will be the slider and the first categories
 function loadFirstView() {
     var homePageSliderWrapper = document.getElementById('HomePageSlider');
-    var homepageSliderTemplate = document.getElementById('HomepageSliderTemplate');
     var urlFeed = homePageSliderWrapper.getAttribute("data-json-feed");
-    homePageSliderWrapper.appendChild(createLoaderForHandlebars());
     axios({
         method:'get',
         url:urlFeed
     })
-    .then(function (response) {            
-        var theScriptHTML = homepageSliderTemplate !== null ? homepageSliderTemplate.innerHTML : "";
-        var theTemplate = Handlebars.compile(theScriptHTML);
-        homePageSliderWrapper.innerHTML = theTemplate(response.data[0]);
+    .then(function (response) {
+        compileDataToHandlebars(response.data[0], homePageSliderWrapper);      
 
         //initialize homepage slider after handlebars template was compiled
         initializeHomepageSlider()
     })
+    .then(function() {
+        //load Recomandari T&T
+        var recomandariWrapper = document.getElementById('RecomandariTT');
+        getDataForHandlebars(recomandariWrapper)
+    })    
     .catch(function (error) {
         // handle error
-        console.log(error, "error boo");
+        console.log(error, "error boo1");
     })
 }
 /*END HOMEPAGE HANDLEBARS AJAX CALLS*/
 
 /*START ADDITIONAL FUNCTIONS*/
+function compileDataToHandlebars(data, homePageSliderWrapper) {
+    var homepageSliderTemplate = document.getElementById(homePageSliderWrapper.getAttribute('data-template'));
+    var theScriptHTML = homepageSliderTemplate !== null ? homepageSliderTemplate.innerHTML : "";
+    var theTemplate = Handlebars.compile(theScriptHTML);
+    homePageSliderWrapper.innerHTML = theTemplate(data);
+}
+//generic function to create call and get data to compile handlebars
+function getDataForHandlebars(homePageSliderWrapper) {
+    var urlFeed = homePageSliderWrapper.getAttribute('data-json-feed')
+    axios({
+        method:'get',
+        url:urlFeed
+    })
+    .then(function (response) {
+        compileDataToHandlebars(response.data[0], homePageSliderWrapper);
+    })
+    .catch(function (error) {
+        // handle error
+        console.log(error, "error boo2");
+    })
+}
 function initializeHomepageSlider() {
     $('.flexslider').flexslider({
         selector: ".featured-slider > .slider-item",
@@ -48,10 +70,4 @@ function initializeHomepageSlider() {
     });
 }
 
-function createLoaderForHandlebars() {
-    var loader = document.createElement('div');
-    loader.classList.add('loader');
-    
-    return loader;
-}
 /*END ADDITIONAL FUNCTIONS*/
